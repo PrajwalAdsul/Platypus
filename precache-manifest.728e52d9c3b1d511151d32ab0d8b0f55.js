1a84001881718337a588ee7768204c94263ea95b
prajwal10 <prajwaladsul@gmail.com>
2020-03-27 01:03:46 +0530

    Updates

---
+++ precache-manifest.728e52d9c3b1d511151d32ab0d8b0f55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1732fae4cb3fcfbfb07b761738ddcab3",
+    "url": "/Platypus/index.html"
+  },
+  {
+    "revision": "aa7a89f37c2bf5a2057a",
+    "url": "/Platypus/static/css/2.d482c13e.chunk.css"
+  },
+  {
+    "revision": "7db4d47ea6793f18de52",
+    "url": "/Platypus/static/css/main.3f3a5dcc.chunk.css"
+  },
+  {
+    "revision": "aa7a89f37c2bf5a2057a",
+    "url": "/Platypus/static/js/2.4975cc53.chunk.js"
+  },
+  {
+    "revision": "a23749f5bc14f6a5b5c1b0c17e40dd53",
+    "url": "/Platypus/static/js/2.4975cc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db4d47ea6793f18de52",
+    "url": "/Platypus/static/js/main.82089e34.chunk.js"
+  },
+  {
+    "revision": "167a2f166cb25b63ab93",
+    "url": "/Platypus/static/js/runtime-main.03ffdaab.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Platypus/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Platypus/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Platypus/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Platypus/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Platypus/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
